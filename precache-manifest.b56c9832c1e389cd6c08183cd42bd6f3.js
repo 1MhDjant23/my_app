b6becfb01ebf87b55b49f176046783af24b1e651
Mohamed Ait Tajante <mait-taj@c2r2p9.1337.ma>
2024-09-29 17:05:44 +0100

    Updates

---
+++ precache-manifest.b56c9832c1e389cd6c08183cd42bd6f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb166c1d7c644cc2342c64570b16fae",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "a2853f06896e4a21be19",
+    "url": "/myapp/static/css/2.04b36542.chunk.css"
+  },
+  {
+    "revision": "6e419eda5cd6187ce36c",
+    "url": "/myapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2853f06896e4a21be19",
+    "url": "/myapp/static/js/2.a811cab2.chunk.js"
+  },
+  {
+    "revision": "ea862b4d7291d9d99556",
+    "url": "/myapp/static/js/3.bd0f349a.chunk.js"
+  },
+  {
+    "revision": "6e419eda5cd6187ce36c",
+    "url": "/myapp/static/js/main.3f233337.chunk.js"
+  },
+  {
+    "revision": "4464dfb91e2ca9d55d62",
+    "url": "/myapp/static/js/runtime~main.92fb6bb8.js"
+  }
+]);
